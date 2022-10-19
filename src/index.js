ae5bbfafe5bc1d09eec7617ff711d7aadf7b7c7a
Jason <jason.mobley5@gmail.com>
2022-10-19 10:07:11 -0400

    split code into separate modules

--- src/index.js
+++ src/index.js
@@ -1,15 +1,4 @@
 import './style.css';
-import { menuToggle, sideMenu } from './listeners';
+import { menuToggle } from './menuToggle';
+import { addTask, closeModal, submitTask } from './addTask';
 
-const addTask = document.getElementById('addTask');
-const closeModal = document.getElementById('closeModal')
-const modal = document.getElementById('modal')
-
-addTask.addEventListener('click', function() {
-    modal.style.visibility = 'visible'
-})
-
-closeModal.addEventListener('click', function() {
-    modal.style.visibility = 'hidden'
-    
-})
