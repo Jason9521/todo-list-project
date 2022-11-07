d3eabb2c97a479c2f72ce68c85c315462080f35c
Jason <jason.mobley5@gmail.com>
2022-11-07 06:08:29 -0500

    Added more imports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+'use strict'
+
 import './style.css';
 import { menuToggle } from './menuToggle';
-import { addTask, closeModal, submitTask } from './addTask';
+import { addTask, closeModal, submitTask, taskArray } from './addTask';
+import { addProject, closeProjectModal, submitProject, projectArray } from './addProject';
 
