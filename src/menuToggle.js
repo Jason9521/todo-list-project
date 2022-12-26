f05b56787f7b92d8178ba6a560a17a2c892b6662
Jason <jason.mobley5@gmail.com>
2022-12-26 14:02:21 -0500

    Deleted whitespace

--- src/menuToggle.js
+++ src/menuToggle.js
@@ -4,7 +4,6 @@ const menuToggle = document.getElementById('menuToggle')
 const sideMenu = document.getElementById('sideMenu')
 const pageInterface = document.getElementById('pageInterface')
 
-
 menuToggle.addEventListener('click', function() {
     sideMenu.classList.toggle('visible')
     pageInterface.classList.toggle('visible')
