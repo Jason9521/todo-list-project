6cc5b34195440aefcf2aef767f5840e7a17d1c75
Jason <jason.mobley5@gmail.com>
2022-12-26 14:00:50 -0500

    Deleted isComplete() from class constructor

--- src/addTask.js
+++ src/addTask.js
@@ -1,14 +1,15 @@
 'use strict'
 
-import { projectArray } from "./addProject"
-export { Task ,addTask, closeModal, submitTask}
+import { Project, storeAllTasks, projectArray, allTasks, storeAllProjects } from "./addProject";
+import { format, parseISO} from "date-fns";
+
+export { Task , taskError, addTask, closeModal, submitTask}
 
 const addTask = document.getElementById('addTask');
 const closeModal = document.getElementById('closeModal');
 const modal = document.getElementById('modal');
 const submitTask = document.getElementById('submitTask');
 const taskTitle = document.getElementById('taskTitle')
-const taskDescription = document.getElementById('taskDescription')
 const taskProject = document.getElementById('taskProject')
 const taskPriority = document.getElementById('taskPriority')
 const dueDate = document.getElementById('dueDate')
@@ -19,79 +20,93 @@ class Task {
     constructor() {
         
         this.title = taskTitle.value;
-        this.description = taskDescription.value;
         this.project = taskProject.value;
         this.priority = taskPriority.value;
         this.dueDate = dueDate.value;
+        this.formattedDate = '';
     }
 
     storeData() {
 
+        allTasks.push(this);
+        storeAllTasks();
+
         projectArray.forEach(project => {
             if (project.title == this.project) {
-                project.storedTasks.push(this)
+                project.storedTasks.push(this);
+                project.loadProject();
             }
         })
     }
 
-    loadData() {
-        // Compile data into an array and display tasks according to stored data
-    }
-
     showError() {
-        taskError.style.visibility = 'hidden'
+        taskError.style.visibility = 'hidden';
     }
 
     taskError() {
 
-        let taskSubmit = false
+        let isTaskDuplicate = false;
+        let fieldCheck = false;
+
+        allTasks.forEach(task => {
+            if (task.title == this.title) {
+                isTaskDuplicate = true;
+                taskError.textContent = 'Cannot use name of existing task';
+                taskError.style.visibility = 'visible';
+                setTimeout(() => {
+                    this.showError()
+                }, 5000);
+            }
+        })
 
         if (this.title == '' || this.title == null) {
-            taskError.textContent = '"Title" field is required'
-            taskError.style.visibility = 'visible'
+            taskError.textContent = '"Title" field is required';
+            taskError.style.visibility = 'visible';
             setTimeout(() => {
                 this.showError()
             }, 5000);
         }
 
-        else if (this.project == '') {
-            taskError.textContent = '"Project" field is required'
-            taskError.style.visibility = 'visible'
-            setTimeout(() => {
-                this.showError()
-            }, 5000);
-        }
+            else if (this.project == '') {
+                taskError.textContent = '"Project" field is required';
+                taskError.style.visibility = 'visible';
+                setTimeout(() => {
+                    this.showError()
+                }, 5000);
+            }
 
-        else if (this.priority == '') {
-            taskError.textContent = '"Priority" field is required'
-            taskError.style.visibility = 'visible'
-            setTimeout(() => {
-                this.showError()
-            }, 5000);
-        }
+            else if (this.priority == '') {
+                taskError.textContent = '"Priority" field is required';
+                taskError.style.visibility = 'visible';
+                setTimeout(() => {
+                    this.showError()
+                }, 5000);
+            }
 
-        else if (this.dueDate == '') {
-            taskError.textContent = '"Due Date" field is required'
-            taskError.style.visibility = 'visible'
-            setTimeout(() => {
-                this.showError()
-            }, 5000);
-        }
+            else if (this.dueDate == '') {
+                taskError.textContent = '"Due Date" field is required';
+                taskError.style.visibility = 'visible';
+                setTimeout(() => {
+                    this.showError()
+                }, 5000);
+            }
 
-        else {
-            taskSubmit = true
-        }
+            else {
+                fieldCheck = true;
+            }
 
-        if (taskSubmit) {
-            this.storeData()
-            this.resetValues()
-            modal.close()
-        }
+        if (fieldCheck && isTaskDuplicate == false) {
+            this.formattedDate = format(parseISO(this.dueDate), 'iiii, PPP');
+            this.storeData();
+            this.resetValues();
+            modal.close();
+            storeAllProjects();
+            storeAllTasks();
+        }  
     }
 
     resetValues() {
         taskTitle.value = ''
-        taskDescription.value = ''
         taskProject.value = ''
         taskPriority.value = ''
         dueDate.value = ''
@@ -99,12 +114,12 @@ class Task {
 }
 
 addTask.addEventListener('click', function() {
-    modal.showModal()
+    modal.showModal();
     modal.style.animation = 'fade-in 0.5s forwards';
 })
 
 closeModal.addEventListener('click', function() {
-    modal.close()
+    modal.close();
 })
 
 submitTask.addEventListener('click', function(e) {
@@ -112,6 +127,3 @@ submitTask.addEventListener('click', function(e) {
     let data = new Task
     data.taskError()
 })
-
-
-
